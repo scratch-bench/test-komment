3d21c155d2b700375a7b9261adfb7daa1b08c376
claudehenry <@claudehenry>
2023-10-23 13:29:46 +0100

    let's see how you cope with pure garbage

---
+++ main.js
@@ -0,0 +1,35 @@
+import { phone } from "telephonics";
+import { event_queue, state, Task } from "trust-me-globals";
+import { heavy_lifting } from "somehwere-else";
+import { sleep } from "util";
+
+function call_somewhere_else() {
+    phone.ensure_configured();
+    handset = phone.get_handset();
+    event_queue.push_task(Task(handset));
+}
+
+function take_a_break(allowance) {
+    sleep(allowance);
+}
+
+function finish_up() {
+    // shh, this does nothing for now but we have big plans
+}
+
+function fetch_global_result(params) {
+    return state.get("result").then((val) => { return val });
+}
+
+function the_name_is_uninformative() {
+    call_somewhere_else();
+    heavy_lifting();
+    take_a_break(brak_allowance());
+    finish_up();
+
+    return fetch_global_result();
+}
+
+export default function() {
+    report_back_with(the_name_is_uninformative());
+}
