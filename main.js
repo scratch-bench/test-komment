cd50ac424a40943b3c259b3e920be3c674ee1813
Claude-Henry <claudehenry@users.noreply.github.com>
2023-11-10 12:04:17 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-/**
- * already documented
- */
+// already documented
 function test() {
     return true;
 }
@@ -15,26 +13,7 @@ function test() {
 function other(delta) {
 }
 
-/**
-* @description This function scales two numbers by a factor of `delta`.
-* 
-* `scale(left = x * (1 - delta), right = y * delta) = x * (1 - delta) + y * delta`.
-* 
-* @param { number } left - The `left` input parameter represents the original value 
-* that should be scaled.
-* 
-* @param {  } right - The `right` input parameter acts as a "scaling factor" that 
-* determines the direction and amount of scaling applied to the `left` parameter.
-* 
-* @param { number } delta - The `delta` input parameter is a scaling factor that 
-* modifies the output of the function by multiplying it with either `left * (1 - 
-* delta)` or `right * delta`, depending on the value of `left` and `right`.
-* 
-* @returns { number } The output returned by this function is:
-* 
-* scale(left=0.5，right=2，delta=1) = 0.5 * (1 - 1) + 2 * 1 = 1
-*/
-function scale(left, right, delta) {
+function lerp(left, right, delta) {
     return left * (1 - delta) + right * delta;
 }
 
