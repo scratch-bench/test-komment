192897bb26c8c306990e39f3bc7be15485b0a70e
Claude-Henry <claudehenry@users.noreply.github.com>
2023-11-10 11:45:49 +0000

    Update main.js

--- main.js
+++ main.js
@@ -5,26 +5,10 @@ function test() {
     return true;
 }
 
-/**
-* @description The function `other()` does nothing (has no implementation) and will 
-* result of `undefined` when called.
-* 
-* 
-* 
-* @returns {  } The output of the `other()` function is `undefined`.
-*/
 function other() {
 }
 
 class Ticker {
-/**
-* @description The function constructs a new object and sets the "count" property 
-* to 0.
-* 
-* 
-* 
-* @returns { number } The output returned by this function is undefined.
-*/
     constructor() {
         this.count = 0;
     }
