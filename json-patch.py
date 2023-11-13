4fd2abe62d3dea63445d52bbf8879d85935f33cc
Claude-Henry <claudehenry@users.noreply.github.com>
2023-11-13 10:24:43 +0000

    Update json-patch.py

--- json-patch.py
+++ json-patch.py
@@ -124,26 +124,6 @@ def apply_patch(doc, patch, in_place=False, pointer_cls=JsonPointer):
 
 
 def make_patch(src, dst, pointer_cls=JsonPointer):
-    """Generates patch by comparing two document objects. Actually is
-    a proxy to :meth:`JsonPatch.from_diff` method.
-
-    :param src: Data source document object.
-    :type src: dict
-
-    :param dst: Data source document object.
-    :type dst: dict
-
-    :param pointer_cls: JSON pointer class to use.
-    :type pointer_cls: Type[JsonPointer]
-
-    >>> src = {'foo': 'bar', 'numbers': [1, 3, 4, 8]}
-    >>> dst = {'baz': 'qux', 'numbers': [1, 4, 7]}
-    >>> patch = make_patch(src, dst)
-    >>> new = patch.apply(src)
-    >>> new == dst
-    True
-    """
-
     return JsonPatch.from_diff(src, dst, pointer_cls=pointer_cls)
 
 
