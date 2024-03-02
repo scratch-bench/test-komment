812283b12d4f06520c3bc47be59386916d172f38
Claude-Henry <claudehenry@users.noreply.github.com>
2024-03-02 14:51:21 +0000

    Update nest.js

--- nest.js
+++ nest.js
@@ -1,20 +1,5 @@
-/**
- * @description This function simply returns the value `0`. It does not perform any
- * complex operations or modifications, and does not rely on any external resources.
- * It is a pure function, with no side effects, and can be safely composed with other
- * functions without worrying about unintended consequences. In short, it is a
- * straightforward and predictable function that always returns the same result.
- * 
- * @returns { integer } The function returns 0.
- */
+
 const X = () => {
-  /**
-   * @description This function returns the value 0. It does not perform any complex
-   * calculations or operations, nor does it access any external resources or data. Its
-   * sole purpose is to return the constant value 0.
-   * 
-   * @returns { number } The function returns 0.
-   */
   const Y = () => {
     return 0;
   }
