c85851bf3fc63ac7686eb8a54db25fa8e36bf636
Claude-Henry <claudehenry@users.noreply.github.com>
2024-03-03 20:11:17 +0000

    Create nest.java

---
+++ nest.java
@@ -0,0 +1,17 @@
+class A {
+    static void B () {
+        class C {
+            static void D() {
+                // imagin this
+            }
+        }
+
+        class E {
+            static void F() {
+                // imagine that
+            }
+        }
+
+        // this and that too
+    }
+}
