cd6583b1604b0ffa4e0f6b6e53450aa56772951f
claudehenry <chrochenoir@gmail.com>
2024-08-27 17:04:57 +0100

    small change 1

--- test.ts
+++ test.ts
@@ -1,46 +1,35 @@
 class A {
-  /**
-   * @description creates a new company in Firestore. It either takes an existing company
-   * object or a WriteBatch object and creates the company in Firestore.
-   * 
-   * @param { WriteBatch | Transaction } .transactionOrBatch - WriteBatch or Transaction
-   * object that is used to create the new company in Firestore, allowing for efficient
-   * batching of multiple operations in a single call.
-   * 
-   * @param { Optional<Company, 'id'> } .company - optional Company object or ID that
-   * is used to create a new company document in Firestore when the `transactionOrBatch`
-   * input is not provided, or to update an existing company document when it is provided.
-   * 
-   * @returns { Company | Promise<Company> } a `Company` object representing the newly
-   * created company, or a promise that resolves to such an object.
-   */
   createCompany = ({
     transactionOrBatch,
     company,
-   }: {
-    transactionOrBatch?: WriteBatch | Transaction
-    company: Optional<Company, 'id'>
-   }): Company | Promise<Company> => {
-     logger.info('creating new company')
-    const companiesRef = firestore.companies()
-    const companyRef = company?.id ? companiesRef.doc(company.id) : companiesRef.doc()
+  }: {
+    transactionOrBatch?: WriteBatch | Transaction;
+    company: Optional<Company, "id">;
+  }): Company | Promise<Company> => {
+    logger.info("creating new company");
+    const companiesRef = firestore.companies();
+    const companyRef = company?.id
+      ? companiesRef.doc(company.id)
+      : companiesRef.doc();
     const completeCompany: Company = {
-     ...company,
-     id: companyRef.id,
-    }
+      ...company,
+      id: companyRef.id,
+    };
     if (transactionOrBatch) {
-     transactionOrBatch.create(companyRef, completeCompany)
-     return completeCompany
+      transactionOrBatch.create(companyRef, completeCompany);
+      return completeCompany;
     }
     return companyRef
-     .create(completeCompany)
-     .then(() => {
-       logger.info(`created new company ${completeCompany.id}`)
-      return completeCompany
-     })
-     .catch((error: Error) => {
-      logger.warn(`failed to create new company ${completeCompany.id}. Error: ${error.message}`)
-      throw error
-     })
- }
+      .create(completeCompany)
+      .then(() => {
+        logger.info(`created new company ${completeCompany.id}`);
+        return completeCompany;
+      })
+      .catch((error: Error) => {
+        logger.warn(
+          `failed to create new company ${completeCompany.id}. Error: ${error.message}`,
+        );
+        throw error;
+      });
+  };
 }
